365fcdd22e44013a4091cf71166cec2c181666cd
Eduardo Morales <emoral435@gmail.com>
2022-10-25 21:22:24 -0500

    Added tailwindcss and react config files with basic background

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className='container mx-auto bg-gray-200 roundex-xl shadow'>
+      <p className='text-3xl text-gray-700 font-bold mb-5'>
+        Welcome!
+      </p>
+      <p className='text-gray-500 text-lg'>
+        React and Tailwind CSS in action
+      </p>
+    </div>
+  )
+}
