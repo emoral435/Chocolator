5f0d2781cb500e9510369fcf5a857019c7716d9f
Eduardo Morales <emoral435@gmail.com>
2022-10-26 21:59:05 -0500

    Added Chocolator Display component

--- src/Components/ChocolatorBigDiv/index.jsx
+++ src/Components/ChocolatorBigDiv/index.jsx
@@ -1,5 +1,5 @@
-export default function ChocolatorDiv() {
-    return (<div classname='text-3xl m-w-[22rem] m-h-[6rem] border-[8px] border-[#674a44] rounded-[10px] box-border p-10 bg-[#452a1d] shadow-2xl'>
+export default function ChocolatorDiv(props) {
+    return (<div className='text-5xl box-border first-line:m-w-[22rem] m-h-[6rem] border-[8px] border-[#674a44] rounded-[10px] p-6 bg-[#452a1d] shadow-2xl '>
         CHOCOLATOR
     </div>
     )
