99f806def0874d5f2fa8cea25b8a563a68a2bf8d
Eduardo Morales <emoral435@gmail.com>
2022-10-27 11:10:59 -0500

    Added credits section along with centering of chocolator and credits section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import NumberGrid from "./Components/NumberGrid"
 import ChocolatorDiv from "./Components/ChocolatorBigDiv"
 import GreenOutput from "./Components/GreenOutput"
+import Credits from "./Components/Credits"
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center">  
-      <div className="bg-[#3b2317]">
-        <NumberGrid />
+    <div className="flex flex-col items-center mt-16">  
+      <div className="bg-[#3b2317] p-12 rounded-[40px] shadow-2xl border border-black">
+        <div className='flex gap-8 pb-16'>
+          <GreenOutput />
+          <ChocolatorDiv />
+        </div>
+        <div>
+          <NumberGrid />
+        </div>
       </div>
-      <div>
-        <ChocolatorDiv />
-      </div>
-      <div>
-        <GreenOutput />
+      <div className='mt-12'>
+        <Credits />
       </div>
     </div>
   )
---
+++ src/Components/Credits/index.jsx
@@ -0,0 +1,15 @@
+import img from './1051377.png';
+
+export default function Credits(props) {
+    return (<div className='flex gap-12'>
+        <div className='flex items-center'>
+            <div className='text-lg'>
+                made by emoral435
+            </div>
+        </div>
+        <a href='https://github.com/emoral435' rel='noreferrer' target='_blank'>
+            <img src={img} alt='github logo' className='w-12 h-12 transition ease-in-out duration-700 hover:rotate-[360deg]' />
+        </a>
+    </div>
+    );
+};
